e7232b23580e64d5317225a520602e1cf0827b41
tbushman <tbushman@pu.bli.sh>
2017-08-02 22:49:53 -0600

    wip vue integration

--- src/project-tests/markdown-previewer-tests.js
+++ src/project-tests/markdown-previewer-tests.js
@@ -1,68 +1,84 @@
 export default function createMarkdownPreviewerTests() {
 
   describe('Markdown Previewer tests', function() {
-    
-    var editor, preview, converter;
+    //var app = require(document.getElementById('thisapp').innerHTML)
+    //console.log(app)
+    //const Vue = require('vue'); 
+    var component, editor, preview, converter;
+    let markdownOnLoad,
+      previewOnLoad, mounted;
+    /*if (!component) {
+      component = require('../../local_test/js/inline.js')
+    }*/
     if (app && app._isVue) {
+      //console.log(component)
       // register
-      /*Vue.component('my-component', Vue.extend({
-        template: document.getElementById('app').innerHTML
+      /*var component_editor = Vue.component('fcc_app', Vue.extend({
+        template: '#fcc_app'//document.getElementById('fcc_app').innerHTML
       }));
       
-      // create a root instance
-      var thisapp = new Vue({
-        el: '#app',
-        name: 'my-component'
-      })*/
-      var component_editor = Vue.component('app', Vue.extend({
-				template: '#app',
-        data: function() {
-    	    return {
-    	     editorContent: {}
-         }
-       }
-     }))
-      var mounted = new Vue(component_editor).$mount()
-      editor = mounted.$refs.editor;
-      preview = mounted.$refs.preview;
-      
+      mounted = new Vue(component_editor).$mount();
+      */
+      var comptemp = {
+        template: document.getElementById('app').innerHTML
+      }
+      mounted = new Vue({
+        /*components: {
+          'my-component': comptemp
+        }*/
+      }).$mount('#app');
+      editor = mounted.$el.querySelector('#editor');
+      preview = mounted.$el.querySelector('#preview');
     } else {
       editor = document.getElementById('editor');
       preview = document.getElementById('preview');
     }
-    let markdownOnLoad,
-      previewOnLoad;
+
+    function currentValue(comp, callback){
+      var comp = comp
+      //Vue.nextTick(comp)
+      Vue.nextTick(function(){
+        callback(comp.$el)
+      });
+    }
+    function triggerChange(str, callback) {
+      // REACT
+      //editor.value = str;
+      //var comp = new component().$mount();
+      /*var comp = new Vue({
+        el: '#thisapp',
+        name: 'my-component',
+        data: function(){
+          return {
+            editorContent: str
+          }
+        }
+      }).$mount()*/
+      /*var component_editor = Vue.component('fcc_app', Vue.extend({
+        template: '#fcc_app'//document.getElementById('fcc_app').innerHTML
+      }));
+      
+      var comp = new Vue(component_editor).$mount();*/
+      var comptemp = {
+        template: document.getElementById('app').innerHTML
+      }
+      var comp = new Vue({
+        
+      }).$mount('#app');
+      comp.editorContent = str;
+      editor = comp.$el.querySelector('#editor');
+      preview = comp.$el.querySelector('#preview');
+      //Vue.nextTick(comp)
+      /*Vue.nextTick(function(){
+        callback(comp.$el)
+      });*/
+      callback()
+    }
     if (editor)
       markdownOnLoad = editor.value;
     if (preview)
       previewOnLoad = preview.innerHTML;
-
-    function triggerChange(str) {
-      // REACT
-      editor.value = str;
-      const eventReact = new Event('input', {bubbles: true});
-      editor.dispatchEvent(eventReact);
-      if (preview.innerHTML === str) {
-        return;
-      } else {
-        const eventJS = new Event('keyup', {bubbles: true}); // must be keyup to live preview
-        if (app && app._isVue) {
-          editor.value = str;
-          var getter = Vue.nextTick(function() {
-            editor = mounted.$refs.editor;
-            preview = mounted.$refs.preview;
-            if (editor) {
-              markdownOnLoad = editor.value;
-            }
-            if (preview) {
-              previewOnLoad = preview.innerHTML;
-            }
-          });
-        }
-        // jQUERY OR JAVASCRIPT
-        editor.dispatchEvent(eventJS)
-      }
-    }
+    
     
 
     describe('#Tests', function() {
@@ -77,22 +93,60 @@ export default function createMarkdownPreviewerTests() {
       });
 
       it('3. When I enter text into the #editor element, the #preview element is updated as I type to display the content of the textarea', function() {
-        triggerChange('a');
-        FCC_Global.assert.strictEqual(preview.innerText.slice(0, 1), 'a', '#preview is not being updated as I type into #editor (should update on every keyup) ');
+        triggerChange('a', function(file){
+          Vue.nextTick(function(){
+            console.log(preview.innerText.slice(0, 1))
+            FCC_Global.assert.strictEqual(preview.innerText.slice(0, 1), 'a', '#preview is not being updated as I type into #editor (should update on every keyup) ');
+          })
+          
+        });
       });
 
-      it('4. When I enter GitHub flavored markdown into the #editor element, the text is rendered as HTML to #preview as I type (Hint: You don\'t need to parse Markdown yourself - you can import the Marked library for this: https://cdnjs.com/libraries/marked)', function() {
+      it('4. When I enter GitHub flavored markdown into the #editor element, the text is rendered as HTML to #preview as I type (Hint: You don\'t need to parse Markdown yourself - you can import the Marked library for this: https://cdnjs.com/libraries/marked)', function(done) {
         const error = 'The markdown in #editor is not being interpreted  correctly and/or rendered into #preview ';
-        triggerChange('');
-        FCC_Global.assert.strictEqual(preview.innerHTML, '', '#preview\'s only children should be those rendered by marked.js ');
-        triggerChange('testing');
-        FCC_Global.assert.strictEqual(preview.innerHTML, '<p>testing</p>\n', error);
-        triggerChange(editor.value + ' and...');
-        FCC_Global.assert.strictEqual(preview.innerHTML, '<p>testing and...</p>\n', error);
-        triggerChange('# h1 \n## h2');
-        FCC_Global.assert.strictEqual(preview.innerHTML, '<h1 id="h1">h1</h1>\n<h2 id="h2">h2</h2>\n', error);
-        triggerChange('**bold**');
-        FCC_Global.assert.strictEqual(preview.innerHTML, '<p><strong>bold</strong></p>\n', error);
+        var comp = new Vue({}).$mount('#app');
+        editor = comp.$el.querySelector('#editor')
+        /*var f = currentValue(editor, function(f){
+          return f;
+        })*/
+        //console.log(f)
+        var files = [
+          {key: '', expected: '', errorMsg: '#preview\'s only children should be those rendered by marked.js '},
+          {key: 'testing', expected: '<p>testing</p>\n', errorMsg: error},
+          {key: editor.value + ' and...', expected: '<p>testing and...</p>\n', errorMsg: error}
+//          {key:}
+        ];
+        for (var i = 0; i < files.length; i++) {
+          //var files = ['', 'testing', f.editor.value + ' and...', '# h1 \n# h2', '**bold**']
+          triggerChange(files[i].key, function(){
+            Vue.nextTick(function(){
+              console.log(editor.innerHTML)
+              FCC_Global.assert.strictEqual(editor.innerHTML, files[i].expected, files[i].errorMsg);          
+            })
+          })
+
+        }
+        done()
+        /*triggerChange('', function(file){
+          console.log('4a :'+file)
+          FCC_Global.assert.strictEqual(file.querySelector('#preview').innerHTML, '', '#preview\'s only children should be those rendered by marked.js ');          
+        });
+        triggerChange('testing', function(file){
+          console.log('4b :'+file)
+          FCC_Global.assert.strictEqual(file.querySelector('#preview').innerHTML, '<p>testing</p>\n', error);          
+        });
+        triggerChange(editor.value + ' and...', function(file){
+          console.log('4c :'+file)
+          FCC_Global.assert.strictEqual(file.querySelector('#preview').innerHTML, '<p>testing and...</p>\n', error);          
+        });
+        triggerChange('# h1 \n## h2', function(file){
+          console.log('4d :'+file)
+          FCC_Global.assert.strictEqual(file.querySelector('#preview').innerHTML, '<h1 id="h1">h1</h1>\n<h2 id="h2">h2</h2>\n', error);
+        });
+        triggerChange('**bold**', function(file){
+          console.log('4e :'+file)
+          FCC_Global.assert.strictEqual(file.querySelector('#preview').innerHTML, '<p><strong>bold</strong></p>\n', error);
+        });*/
       });
 
       it('5. When my markdown previewer first loads, the #editor field should contain valid default markdown that provides a brief description of the project and demonstrates the previewer\'s capabilities', function() {
@@ -101,58 +155,63 @@ export default function createMarkdownPreviewerTests() {
       });
 
       it('6. When my markdown previewer first loads, the default text in the #editor field should contain valid markdown that represents at least one of each of the following elements: a header (H1 size), a sub header (H2 size), a link, inline code, a code block, a list item, a blockquote, an image, and bolded text', function() {
-        triggerChange(markdownOnLoad);
-        const markdown = editor.value;
+        triggerChange(markdownOnLoad, function(file){
+          console.log('6 :'+file)
 
-        FCC_Global.assert.notStrictEqual(markdown.search(/#\s.+/), -1, 'write some markdown representing an <h1> '); // h1
-        FCC_Global.assert.notStrictEqual(markdown.search(/##\s.+/), -1, 'write some markdown representing an <h2> '); // h2
-        FCC_Global.assert.notStrictEqual(markdown.search(/\[.+\]\(.+\..+\)/), -1, 'write some markdown representing an <a> '); // link
-        FCC_Global.assert.notStrictEqual(markdown.search(/`.+`/), -1, 'write some markdown representing inline <code> '); // inline code
-        FCC_Global.assert.notStrictEqual(markdown.search(/```[^]+```/), -1, 'write some markdown representing a codeblock, i.e. <pre><code>...</code></pre> '); // codeblock
-        FCC_Global.assert.notStrictEqual(markdown.search(/(?:-|\d\.)\s[^|\s-*].+/), -1, 'write some markdown representing an <li> item '); // ol or ul list item
-        FCC_Global.assert.notStrictEqual(markdown.search(/>\s.+/), -1, 'write some markdown representing a <blockquote> '); // blockquote
-        FCC_Global.assert.notStrictEqual(markdown.search(/!\[.*\]\(.+\..+\)/), -1, 'write some markdown representing an <image> '); // image
-        FCC_Global.assert.notStrictEqual(markdown.search(/(\*\*|__).+\1/), -1, 'write some markdown representing <strong> text '); // bold text
+          const markdown = file.querySelector('#editor').value;
+
+          FCC_Global.assert.notStrictEqual(markdown.search(/#\s.+/), -1, 'write some markdown representing an <h1> '); // h1
+          FCC_Global.assert.notStrictEqual(markdown.search(/##\s.+/), -1, 'write some markdown representing an <h2> '); // h2
+          FCC_Global.assert.notStrictEqual(markdown.search(/\[.+\]\(.+\..+\)/), -1, 'write some markdown representing an <a> '); // link
+          FCC_Global.assert.notStrictEqual(markdown.search(/`.+`/), -1, 'write some markdown representing inline <code> '); // inline code
+          FCC_Global.assert.notStrictEqual(markdown.search(/```[^]+```/), -1, 'write some markdown representing a codeblock, i.e. <pre><code>...</code></pre> '); // codeblock
+          FCC_Global.assert.notStrictEqual(markdown.search(/(?:-|\d\.)\s[^|\s-*].+/), -1, 'write some markdown representing an <li> item '); // ol or ul list item
+          FCC_Global.assert.notStrictEqual(markdown.search(/>\s.+/), -1, 'write some markdown representing a <blockquote> '); // blockquote
+          FCC_Global.assert.notStrictEqual(markdown.search(/!\[.*\]\(.+\..+\)/), -1, 'write some markdown representing an <image> '); // image
+          FCC_Global.assert.notStrictEqual(markdown.search(/(\*\*|__).+\1/), -1, 'write some markdown representing <strong> text '); // bold text
+        });
       });
 
       it('7. When my markdown previewer first loads, the default markdown in the #editor field should be rendered as HTML in the #preview element', function() {
-        triggerChange(markdownOnLoad);
-        FCC_Global.assert.notStrictEqual(preview.innerHTML, '', '#preview should have inner HTML ');
-        FCC_Global.assert.strictEqual(preview.innerHTML, previewOnLoad, '#editor\'s  markdown does not render correctly on window load ');
-        const markdown = editor.value;
-        // this could be significantly shortened into one test, however feedback would not be specific
-        FCC_Global.assert.isAtLeast(document.querySelectorAll('#preview h1').length, 1, '#preview does not contain at least one <h1> ');
-        FCC_Global.assert.isAtLeast(document.querySelectorAll('#preview h2').length, 1, '#preview does not contain at least one <h2> ');
-        FCC_Global.assert.isAtLeast(document.querySelectorAll('#preview a').length, 1, '#preview does not contain at least one <a> ');
-        FCC_Global.assert.isAtLeast(document.querySelectorAll('#preview code').length, 1, '#preview does not contain at least one <code> ');
-        FCC_Global.assert.isAtLeast(document.querySelectorAll('#preview pre').length, 1, '#preview does not contain at least one <pre> ');
-        FCC_Global.assert.isAtLeast(document.querySelectorAll('#preview li').length, 1, '#preview does not contain at least one <li> ');
-        FCC_Global.assert.isAtLeast(document.querySelectorAll('#preview blockquote').length, 1, '#preview does not contain at least one <blockquote> ');
-        FCC_Global.assert.isAtLeast(document.querySelectorAll('#preview img').length, 1, '#preview does not contain at least one <img> ');
-        FCC_Global.assert.isAtLeast(document.querySelectorAll('#preview strong').length, 1, '#preview does not contain at least one <strong> ');
+        triggerChange(markdownOnLoad, function(file){
+          console.log('7 :'+file)
+          FCC_Global.assert.notStrictEqual(file.querySelector('#preview').innerHTML, '', '#preview should have inner HTML ');
+          FCC_Global.assert.strictEqual(file.querySelector('#preview').innerHTML, previewOnLoad, '#editor\'s  markdown does not render correctly on window load ');
+          const markdown = file.querySelector('#editor').value;
+          // this could be significantly shortened into one test, however feedback would not be specific
+          FCC_Global.assert.isAtLeast(document.querySelectorAll('#preview h1').length, 1, '#preview does not contain at least one <h1> ');
+          FCC_Global.assert.isAtLeast(document.querySelectorAll('#preview h2').length, 1, '#preview does not contain at least one <h2> ');
+          FCC_Global.assert.isAtLeast(document.querySelectorAll('#preview a').length, 1, '#preview does not contain at least one <a> ');
+          FCC_Global.assert.isAtLeast(document.querySelectorAll('#preview code').length, 1, '#preview does not contain at least one <code> ');
+          FCC_Global.assert.isAtLeast(document.querySelectorAll('#preview pre').length, 1, '#preview does not contain at least one <pre> ');
+          FCC_Global.assert.isAtLeast(document.querySelectorAll('#preview li').length, 1, '#preview does not contain at least one <li> ');
+          FCC_Global.assert.isAtLeast(document.querySelectorAll('#preview blockquote').length, 1, '#preview does not contain at least one <blockquote> ');
+          FCC_Global.assert.isAtLeast(document.querySelectorAll('#preview img').length, 1, '#preview does not contain at least one <img> ');
+          FCC_Global.assert.isAtLeast(document.querySelectorAll('#preview strong').length, 1, '#preview does not contain at least one <strong> ');
 
-        // then check a couple of elements to make sure the present elements
-        //are actually the ones represented by the markdown:
+          // then check a couple of elements to make sure the present elements
+          //are actually the ones represented by the markdown:
 
-        // find matching H1 element
-        const h1Text = /#\s.*/.exec(markdown)[0].slice(2);
-        const h1Match = [];
-        document.querySelectorAll('#preview h1').forEach(h1 => {
-          if (h1.innerText === h1Text)
-            h1Match.push(h1);
-          }
-        );
-        FCC_Global.assert.isAtLeast(h1Match.length, 1, '#preview does not contain the H1 element represented by the markdown in the #editor field with the inner text ' + h1Text + ' ');
+          // find matching H1 element
+          const h1Text = /#\s.*/.exec(markdown)[0].slice(2);
+          const h1Match = [];
+          document.querySelectorAll('#preview h1').forEach(h1 => {
+            if (h1.innerText === h1Text)
+              h1Match.push(h1);
+            }
+          );
+          FCC_Global.assert.isAtLeast(h1Match.length, 1, '#preview does not contain the H1 element represented by the markdown in the #editor field with the inner text ' + h1Text + ' ');
 
-        // find matching H2 element
-        const h2Text = /##\s.*/.exec(markdown)[0].slice(3);
-        const h2Match = [];
-        document.querySelectorAll('#preview h2').forEach(h2 => {
-          if (h2.innerText === h2Text)
-            h2Match.push(h2);
-          }
-        );
-        FCC_Global.assert.isAtLeast(h2Match.length, 1, '#preview does not contain the H2 element represented by the markdown in the #editor field with the inner text ' + h2Text + ' ');
+          // find matching H2 element
+          const h2Text = /##\s.*/.exec(markdown)[0].slice(3);
+          const h2Match = [];
+          document.querySelectorAll('#preview h2').forEach(h2 => {
+            if (h2.innerText === h2Text)
+              h2Match.push(h2);
+            }
+          );
+          FCC_Global.assert.isAtLeast(h2Match.length, 1, '#preview does not contain the H2 element represented by the markdown in the #editor field with the inner text ' + h2Text + ' ');
+        });
 
       });
 
--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,11 +1,57 @@
 const PROJECT_PATH = __dirname + '/local_test'
-module.exports = {
+/*module.exports = {
     entry: "./src/index.js",
     output: {
         library: 'FCC_Global',
         path: PROJECT_PATH + '/js',
         filename: "bundle.js"
     },
+    /*entry: "./local_test/js/index.js",
+    output: {
+      path: "./local_test/js",
+      filename: "inline.js"
+    },*/
+    /*module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+                query: {
+                    presets: ['es2015']
+                }
+            }/*,
+            {
+                test: /\.vue$/,
+                loader: 'vue-loader'/*,
+                options: {
+                  loaders: {
+                  }
+                  // other vue-loader options go here
+                }*/
+            //}
+        /*]
+    }/*,
+    vue: {
+      loaders: {
+        js: 'babel-loader'
+      }
+    },
+    resolve: {
+      alias: {
+        'vue$': 'vue/dist/vue.esm.js'
+    }*/
+  /*}
+};*/
+
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+      library: 'FCC_Global',
+      path: PROJECT_PATH + '/js',
+      filename: "bundle.js"
+  },
     module: {
         loaders: [
             {
@@ -15,7 +61,7 @@ module.exports = {
                 query: {
                     presets: ['es2015']
                 }
-            }       
+            } 
         ]
     }
 };
